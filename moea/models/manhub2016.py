ec61305a007434586d4782d28fa32891eaefe4e8
Michele Urbani <murbani@fbk.eu>
2024-09-24 17:13:57 +0200

    Added constraints to Manhub2016.

--- moea/models/manhub2016.py
+++ moea/models/manhub2016.py
@@ -1,10 +1,12 @@
+import numpy as np
 from typing import Union
 from pathlib import Path
 
 from pymoo.core.variable import Real
 
 
-from moea.utils import dump_input, find_values, execute_energyplan_spool
+from moea.utils import (dump_input, find_values, execute_energyplan_spool,
+                        parse_output)
 from moea.config import ENERGYPLAN_RESULTS
 from moea.models.base_model import BaseModel
 
@@ -13,7 +15,10 @@ class Manhub2016(BaseModel):
     """
     This problem class replicates the model in
 
-    > Mahbub, M. S., Cozzini, M., Østergaard, P. A., Alberti, F. (2016). Combining multi-objective evolutionary algorithms and descriptive analytical modelling in energy scenario design. Applied Energy, 164, 140-151.
+    > Mahbub, M. S., Cozzini, M., Østergaard, P. A., Alberti, F. (2016).
+    > Combining multi-objective evolutionary algorithms and descriptive
+    > analytical modelling in energy scenario design. *Applied Energy*,
+    > 164, 140-151.
 
     There are six decision variables, which are the capacities of power
     plants, CHP, heat pump, onshore wind, offshore wind, and PV. The
@@ -87,6 +92,56 @@ class Manhub2016(BaseModel):
             ENERGYPLAN_RESULTS,
             *self.objectives
         )
-        # Store the constraint violation in the output dictionary. This is not
-        # used in this problem, but it is included for completeness.
-        # out["G"] = 0
+
+        # CONSTRAINTS
+        # Since there are a fes constraints, we evaluate the left-hand side
+        # of the constraints and store the values in out["G"].
+
+        # Read and store the values of interest into arrays
+        import_elec = []
+        heat_prod, heat_demand = [], []
+        stable_load = []
+        for out in ENERGYPLAN_RESULTS.glob(".txt"):
+            # Parse the output file
+            D = parse_output(out)
+            # Results are organized as dictionary, one for each section, and
+            # each value is a pandas DataFrame
+            montly_lbl = 'MONTHLY AVERAGE VALUES (MW)'
+            # Retrieve import
+            import_elec.append(
+                D[montly_lbl]['Import Electr.']['Annual Maximum'] \
+                    .astype(float))
+            # Retrieve Balance3 heat
+            heat_prod.append(
+                D['TOTAL FOR ONE YEAR (TWh/year)']['Balance3  heat'] \
+                    .astype(float))
+            # Retrieve heat demand
+            heat_demand.append(
+                D[montly_lbl]['DH Demand'].astype(float).sum())
+            # Retrieve stabilized load percentage
+            stable_load.append(
+                D[montly_lbl]['Stabil. Load']['Annual Minimum'].astype(float))
+
+        # Transmission line capacity of export/import: 160 MW. This constraint
+        # enforces the system to produce enough electricity so that it does not
+        # require to import more than 160 MW.
+        import_constr = np.array(import_elec) - 160
+
+        # Heat balance. This constraint enforces the system to produce exactly
+        # the amount of heat necessary to meet the heat demand.
+        heat = np.array(heat_demand) - np.array(heat_prod)
+
+        # Grid stabilization: More than 30% of power production in all hours
+        # must come from units able to supply grid support (see [77] for
+        # details on grid stability in EnergyPLAN).
+        # This constraint is already taken into account by EnergyPLAN
+        stable_load = 0.3 - np.array(stable_load) / 100
+
+        out["G"] = np.concatenate([
+            import_constr,
+            stable_load,
+        ])
+
+        out["H"] = np.array([
+            heat,
+        ])
