46f06ee56a0941b9bc978445a56c4b76735d75fe
Michele Urbani <murbani@fbk.eu>
2024-12-06 11:52:11 +0100

    Changed Aalborg for new read output method.

--- tests/test_algorithms.py
+++ tests/test_algorithms.py
@@ -6,11 +6,11 @@ from moea.models import get_model
 from moea.algorithms import get_algorithm
 
 
-class TestAalborg(unittest.TestCase):
+class TestAalborgA(unittest.TestCase):
 
     def setUp(self) -> None:
         self.model = get_model('AalborgA')
-        self.algorithm = get_algorithm('NSGAII', pop_size=10)
+        self.algorithm = get_algorithm('NSGAII', pop_size=5)
         return super().setUp()
 
     def test_model_features(self):
@@ -22,10 +22,39 @@ class TestAalborg(unittest.TestCase):
         res = minimize(
             problem=self.model,
             algorithm=self.algorithm,
-            termination=('n_gen', 10),
+            termination=('n_gen', 3),
             seed=1
         )
         self.assertIsNotNone(res.X)
         self.assertIsNotNone(res.F)
         # Check that at least one non-dominated solution is found
         self.assertGreaterEqual(res.F.shape[0], 1)
+
+
+class TestAalborgB(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.model = get_model('AalborgB')
+        self.algorithm = get_algorithm('NSGAII', pop_size=5)
+        return super().setUp()
+
+    def test_model_features(self):
+        self.assertEqual(self.model.n_var, 7)
+        self.assertEqual(self.model.n_obj, 2)
+        self.assertEqual(self.model.n_ieq_constr, 3)
+
+    def test_execution(self):
+        res = minimize(
+            problem=self.model,
+            algorithm=self.algorithm,
+            termination=('n_gen', 3),
+            seed=1
+        )
+        self.assertIsNotNone(res.X)
+        self.assertIsNotNone(res.F)
+        # Check that at least one non-dominated solution is found
+        self.assertGreaterEqual(res.F.shape[0], 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
