3fdeec64577a6163b969259a2736a6bae4487504
Michele Urbani <murbani@fbk.eu>
2025-01-21 15:52:53 +0100

    Added debug file and VS code debug config.

--- moea/models/__init__.py
+++ moea/models/__init__.py
@@ -4,12 +4,14 @@ def get_model(name, *args, **kwargs):
     from moea.models.aalborg import AalborgA, AalborgB
     from moea.models.giudicarie import GiudicarieEsteriori
     from moea.models.vdn import ValDiNon
+    from moea.models.oslo import Oslo
 
     PROBLEM = {
         'aalborga': AalborgA,
         'aalborgb': AalborgB,
         'giudicarie': GiudicarieEsteriori,
-        'vdn': ValDiNon
+        'vdn': ValDiNon,
+        'oslo': Oslo,
     }
 
     if name not in PROBLEM:
