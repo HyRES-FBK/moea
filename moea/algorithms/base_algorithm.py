b2b211daaac68d90c5750dacb0af5dc70ef2b31b
Michele Urbani <murbani@fbk.eu>
2025-02-06 11:59:49 +0100

    Changed default parameters for base algorithm.

--- moea/algorithms/base_algorithm.py
+++ moea/algorithms/base_algorithm.py
@@ -1,9 +1,21 @@
 from moea.utils import setup_spool_folder, setup_results_folder
 
 from pymoo.algorithms.moo.nsga2 import NSGA2
+from pymoo.operators.mutation.pm import PM
+from pymoo.operators.crossover.sbx import SBX
+from pymoo.operators.survival.rank_and_crowding import RankAndCrowding
 
 
 class BaseAlgorithm(NSGA2):
+
+    def __init__(self, pop_size=100, **kwargs):
+        super().__init__(
+            pop_size,
+            crossover=SBX(eta=10, prob=0.9),
+            mutation=PM(eta=10, prob=1/7),
+            survival=RankAndCrowding(),
+            **kwargs
+        )
 
     def __str__(self) -> str:
         return self.__class__.__name__
